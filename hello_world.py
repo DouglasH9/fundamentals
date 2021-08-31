fcf2e25762a055f407c8c5c519087704f72ac933
Doug Herman <douglash9882@gmail.com>
2021-08-30 22:19:21 -0500

    hello world python assignment

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+name =  "Noelle"
+
+print("Hello", name)
+print("hello " + name)
+
+name =  42
+print("Hello", str(name))
+print("Hello " + str(name))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+
+print ("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print (f"I love to eat {fave_food1} and {fave_food2}")
